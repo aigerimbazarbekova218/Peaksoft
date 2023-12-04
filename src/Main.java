a298bf129f0f348bc99e2f090bf3c332f65ef142
aiger <aigerim.bazarbekova218@gmail.com>
2023-12-04 20:38:44 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        String name="Peaksoft House";
+        int age=3;
+        String greeting="Туулган кунун менен,"+name+"!";
+        System.out.println(greeting);
+        System.out.println(age+"жашка толушун менен");
+        System.out.println("Куттуктайбыз жана ийгилик каалайм!");
+        System.out.println("Hello world!");
+    }
+}
